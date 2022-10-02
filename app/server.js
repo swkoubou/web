d8619ed876e47e53b7e678fa9dd74a5b099b2c88
x0y14 <ya01ma08@gmail.com>
2022-10-02 14:28:58 +0900

    membersページ追加

--- app/server.js
+++ app/server.js
@@ -1,28 +1,27 @@
-const http = require('http');
-const fs = require('fs');
+// 参考: https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
+const express = require('express')
+const path = require('path');
 
-const port = 8080;
+// アプリを生成
+const app = express()
+// ポート設定
+const port = process.env.PORT || 8080;
 
+// 静的なファイル提供用ディレクトリを設定
+app.use(express.static(path.join(__dirname, 'public')))
 
-var server = http.createServer(
-    (_ ,response) => {
-        fs.readFile('./app/html/index.html', 'utf8', (error, data) => {
-            if (error != null) {
-                console.log("faield to read file: ", error);
-                response.writeHead(500, {'Content-Type':'text/html'});
-                response.write("500 internal error");
-                response.end();
-                return;
-            }
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.write(data);
-            response.end();
-            return;
-        })
-       
-    }
-);
+// ルーティング
+// topページ
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
+// membersページ
+app.get('/members', function (req, res) {
+    res.sendFile(path.join(__dirname, 'pages/members.html'));
+})
 
 
+// アプリ起動
 console.log("listening: http://127.0.0.1:%d", port);
-server.listen(port);
+app.listen(port)
+
